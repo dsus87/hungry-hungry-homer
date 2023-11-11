b4736d9811d246a453778fbd617abcf3b4c014eb
Ana Filipa <anafilipa737@gmail.com>
2023-11-11 11:27:37 +0000

    script file

--- js/script.js
+++ js/script.js
@@ -2,8 +2,15 @@ window.onload = function () {
     const startButton = document.getElementById("start-button");
     const restartButton = document.getElementById("restart-button");
     let game;
-  
-  
+
+    startButton.addEventListener("click", function () {
+      startGame();
+    });
+
+    restartButton.addEventListener("click", function () {
+      restartGame();
+    });
+   
     function startGame() {
       console.log("start game");
       game = new Game(); // create an instance of the Game class
@@ -13,19 +20,17 @@ window.onload = function () {
     function restartGame() {
       location.reload();
     }
-  
+   
     function handleKeydown(event) {
       const key = event.key;
       const possibleKeystrokes = [
         "ArrowLeft",
         "ArrowRight",
       ];
-  
-      // Check if the pressed key is in the possibleKeystrokes array
+       
       if (possibleKeystrokes.includes(key)) {
         event.preventDefault();
-  
-        // Update player's directionX and directionY based on the key pressed
+         
         switch (key) {
           case "ArrowLeft":
             game.player.directionX = -1;
@@ -37,16 +42,7 @@ window.onload = function () {
       }
     }
   
-    startButton.addEventListener("click", function () {
-      startGame();
-    });
   
-    restartButton.addEventListener("click", function () {
-      // Call the restartGame function when the button is clicked
-      restartGame();
-    });
-  
-     // Add the handleKeydown function as an event listener for the keydown event
      window.addEventListener("keydown", handleKeydown);
   };
   
