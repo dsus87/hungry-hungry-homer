8efa36d06f6834902ae6e5fd3d0e6f20d6b07c2e
Omayma El Hadari <om.elhadari@gmail.com>
2023-11-11 12:36:24 +0100

    game changes

--- js/game.js
+++ js/game.js
@@ -1,44 +1,115 @@
-class Game{
+class Game {
+    // code to be added
     constructor(){
         this.startScreen = document.getElementById('game-intro');
         this.gameScreen = document.getElementById('game-screen');
         this.gameEndScreen = document.getElementById('game-end');
-        this.player= new Player(
+        this.player = new Player(
             this.gameScreen,
-            0,
-            0,
-            0,
-            0,
+            1000,
+            680,
+            68.5,
+            150,
             './images/homer-character.png'
-        );
+        ); //  new Player()
         this.width = 1280;
         this.height = 720;
-        this.obstacles = [];
+        this.obstacles = []; // new Obstacle()
         this.score = 0;
         this.lives = 3;
         this.gameIsOver = false;
     }
-}
 
-start() {
-
-    this.gameScreen.style.width = `${this.width}px`;
-    this.gameSceen.style.height = `${this.height}px`;
-    this.startScreen.style.display = 'none';
-    this.gameScreen.style.display = 'block';
-    this.gameloop();
-}
-
-gameLoop(){
-    if (this.gameIsOver === true){
-        return;
+    start(){
+        
+        this.gameScreen.style.width = `${this.width}px`;
+        this.gameScreen.style.height = `${this.height}px`;
+        this.startScreen.style.display = 'none';
+        this.gameScreen.style.display = 'block';
+        this.gameLoop();
+        // Car start at a specific position
+        // Obstacles are going to be at a specific position as well
     }
-    this.update();
-    window.requestAnimationFrame(()=> this.gameLoop())
-}
 
-update(){
-    
+    gameLoop(){
+        // Right now, always this.gameIsOver === false
+        if(this.gameIsOver === true){
+            return;
+        }
+        console.log('gameLoop exec')
+        this.update();// update the game
+        // this.gameLoop()
+        window.requestAnimationFrame(()=>  this.gameLoop()); // used to improve/better manage the rate of frames for the game animation
+    }
+
+    update(){
+        // Return the new position of the car to update the game
+        this.player.move();
+        // Return the new positions of the obstacles to update the game
+       for(let i= 0; i < this.obstacles.length; i++){
+        const obstacle = this.obstacles[i];
+        obstacle.move();
+
+        // If the player's car collides with an obstacle
+            if (this.player.didCollide(obstacle)) {
+                // Remove the obstacle element from the DOM
+                obstacle.element.remove();
+                // Remove obstacle object from the array
+                this.obstacles.splice(i, 1);
+                // Reduce player's lives by 1
+                if (obstacle === Donut){
+                    this.score += this.points;
+                }else if(obstacle=== Sandwich){
+                    this.score += this.points
+                }else if(obstacle=== Beer){
+                    this.score += this.points;
+                }else if(obstacle=== pomatoJuice){
+                    this.lives--
+                }else if (obstacle===){
+
+                }
+                this.lives--;
+                document.getElementById('lives').textContent = this.lives;
+                // Update the counter variable to account for the removed obstacle
+                i--;
+            } 
+
+            /*else if (obstacle.top > this.height) {
+                // Increase the score by 1
+                this.score++;
+                document.getElementById('score').textContent = this.score;
+                // Remove the obstacle from the DOM
+                obstacle.element.remove();
+                // Remove obstacle object from the array
+                this.obstacles.splice(i, 1);
+                // Update the counter variable to account for the removed obstacle
+                i--;
+            }*/
+        }
+      // End the game
+        if (this.lives === 0) {
+            this.endGame();
+        }
+    // Create a new obstacle based on a random probability
+    // when there is no other obstacles on the screen
+        if (Math.random() > 0.98 && this.obstacles.length < 1) {
+                this.obstacles.push(new Obstacle(this.gameScreen));
+        }
+       
+    }
+
+    // Create a new method responsible for ending the game
+  endGame() {
+    this.player.element.remove(); // remove the player car from the screen
+    this.obstacles.forEach(obstacle => obstacle.element.remove()); // remove the obstacles from the screen
+
+    this.gameIsOver = true; // cancel the execution of gameLoop()
+
+    // Hide game screen
+    this.gameScreen.style.display = "none";
+    // Show end game screen
+    this.gameEndScreen.style.display = "block";
+  }
 }
 /* 
  */
