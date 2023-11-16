048d2e5e1c3b30c0f3ef856013b8e5ff14f121b2
Omayma El Hadari <om.elhadari@gmail.com>
2023-11-16 13:35:46 +0100

    last changes, clean code, final comments

--- js/player.js
+++ js/player.js
@@ -1,72 +1,68 @@
 class Player {
-    // to be defined later on
-    constructor(gameScreen, left, top, width, height, imgSrc){
-        this.gameScreen = gameScreen;
-        this.left = left;
-        this.top = top;
-        this.width = width;
-        this.height = height;
-        this.directionX = 0;
-        this.directionY = 0;
-        this.element = document.createElement('img');
+  // file where we gonna take care of players functionality
+  constructor(gameScreen, left, top, width, height, imgSrc){
+      this.gameScreen = gameScreen;
+      this.left = left;
+      this.top = top;
+      this.width = width;
+      this.height = height;
+      this.directionX = 0;
+      this.directionY = 0;
+      //using DOM to add our character Homer which is going to be child of gameScreen div
+      this.element = document.createElement('img');
+      this.element.src = imgSrc;
+      this.element.style.position = 'absolute';
+      this.element.style.width = `${width}px`;
+      this.element.style.height = `${height}px`;
+      this.element.style.top = `${top}px`;
+      this.element.style.left = `${left}px`;
+      this.gameScreen.appendChild(this.element);
+  }
 
-        this.element.src = imgSrc;
-        this.element.style.position = 'absolute';
-        this.element.style.width = `${width}px`;
-        this.element.style.height = `${height}px`;
-        this.element.style.top = `${top}px`;
-        this.element.style.left = `${left}px`;
-        
-        this.gameScreen.appendChild(this.element);
-    }
-
-    move(){
-        // Update player's position based on directionX and directionY
+  move(){
+      // Update player's position based on directionX and directionY
     this.left += this.directionX;
     this.top += this.directionY;
 
-    if (this.left < 10) {
+      //for the player not to get out of the left side game frame
+      if (this.left < 10) {
         this.left = 10;
       }
-  
-      // handles top side
-      if (this.top < 10) {
-        this.top = 10;
-      }
-  
-      // handles right hand side
+
+      // for the player not to get out of the right side game frame
       if (this.left > this.gameScreen.offsetWidth - this.width - 10) {
         this.left = this.gameScreen.offsetWidth - this.width - 10;
-      }
-  
-      // handles bottom side
-      if (this.top > this.gameScreen.offsetHeight - this.height - 10) {
-        this.top = this.gameScreen.offsetHeight - this.height - 10;
-      }
+      };
 
-    // Update the player's car position on the screen
+    // Update hommers' position on the screen
     this.updatePosition();
-    }
+  }
 
-    updatePosition(){
-        this.element.style.left = `${this.left}px`;
-        this.element.style.top = `${this.top}px`;
-        console.log('player position', this.element.getBoundingClientRect())
-    }
+  //updating position after changing position values with .move() method
+  updatePosition(){
+      this.element.style.left = `${this.left}px`;
+      this.element.style.top = `${this.top}px`;
+      console.log('player position', this.element.getBoundingClientRect())
+  }
 
-    didCollide(obstacle) {
-      const playerRect = this.element.getBoundingClientRect();
-      const obstacleRect = obstacle.element.getBoundingClientRect();
-  
-      if (
-        playerRect.left < obstacleRect.right &&
-        playerRect.right > obstacleRect.left &&
-        playerRect.top < obstacleRect.bottom &&
-        playerRect.bottom > obstacleRect.top
-      ) {
-        return true;
-      } else {
-        return false;
-      }
+  //establish the conditions of collision
+  didCollide(obstacle) {
+
+    //.getBoundingCLientRect() method returns the elements size and its relative position on the screen
+    const playerRect = this.element.getBoundingClientRect();
+    const obstacleRect = obstacle.element.getBoundingClientRect();
+
+    if (
+      //when obstacle collides with the player from the players' right side
+      playerRect.left < obstacleRect.right &&
+      //when the obstacle collides with the player from the playerr' left side 
+      playerRect.right > obstacleRect.left &&
+      //when the obstacle collides with the player from the upper side of the player
+      playerRect.bottom > obstacleRect.top
+    ) {
+      return true; // obstacle and player did collide if any of the statements inside the if conditional occurs
+    } else {
+      return false; // collision didn't occur
     }
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,82 +1,86 @@
 window.onload = function () {
-    const startButton = document.getElementById("start-button");
-    const restartButton = document.getElementById("restart-button");
-    let game;
-    
-
-    startButton.addEventListener("click", function () {
-      startGame();
-      audio.play();
-      
-    });
-
-    restartButton.addEventListener("click", function () {
-      restartGame();
-    });
-
-    let movementSpeedLeft = -1.5;
-    let movementSpeedRight = 1.5;
-
-   
-    function startGame() {
-      console.log("start game");
-      game = new Game(); // create an instance of the Game class
-      game.start(); // execute the start method
-      
-
-    setInterval(function() {
-      movementSpeedLeft  -= 1.5; 
-      movementSpeedRight += 1.5; 
-    }, 7500); 
-      
-    }
+  const startButton = document.getElementById("start-button");
+  const restartButton = document.getElementById("restart-button");
+  let game;
   
-    function restartGame() {
-      location.reload();
-    }
-   
-    function handleKeydown(event) {
-      const key = event.key;
-      const possibleKeystrokes = [
-        "ArrowLeft",
-        "ArrowRight",
-      ];
+  //add click event listeners to the buttons for starting/restarting the game
+  startButton.addEventListener("click", function () {
+    startGame();
+    audio.play();
+    
+  });
+
+  restartButton.addEventListener("click", function () {
+    restartGame();
+  });
+
+  let movementSpeedLeft = -1.5;
+  let movementSpeedRight = 1.5;
+
+ 
+  function startGame() {
+    console.log("start game");
+    game = new Game(); // create an instance of the Game class
+    game.start(); // execute the start method
+    
+      //for giving some difficulty to the game, the players' speed is going to increase every 7.5 seconds
+      setInterval(function() {
+        movementSpeedLeft  -= 1.5; 
+        movementSpeedRight += 1.5; 
+      }, 7500); 
+    
+  }
+
+  function restartGame() {
+    //refresh the page which means the game restarts
+    location.reload();
+  }
+ 
+    // setting interactions with the keybord, gonna use left and right arrows
+  function handleKeydown(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowRight",
+    ];
+     
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault(); //if the event does not take place -stroking a key- the pages' default action its not gonna happen
        
-      if (possibleKeystrokes.includes(key)) {
-        event.preventDefault();
-         
-        switch (key) {
-          case "ArrowLeft":
-            game.player.directionX = movementSpeedLeft;
-            break;
-         case "ArrowRight":
-            game.player.directionX = movementSpeedRight;
-            break;
-        }
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = movementSpeedLeft;
+          break;
+       case "ArrowRight":
+          game.player.directionX = movementSpeedRight;
+          break;
       }
     }
+  }
+  //adressing the window (our page or document let's say) and adding an event listener -which is stroking down a key- and telling it 
+  //to run our handlekeydown function when it happens. Basically it's gonna check if the stroken key are the left/right arrows or not.
+  // If so, players gonna change it's position.
   
-  
-     window.addEventListener("keydown", handleKeydown);
+   window.addEventListener("keydown", handleKeydown);
 
-  };
+};
 
-  
-  // Music
-  let audio = new Audio("./sound/intro-end-song.mp3");
-  audio.volume = 0.1;
 
-  let soundDonut = new Audio("./sound/mmm-donuts.mp3");
-  soundDonut.volume = 0.1;
+// Music
+let audio = new Audio("./sound/intro-end-song.mp3");
+audio.volume = 0.1;
 
-  let soundBeer = new Audio("./sound/mmm-beer.mp3");
-  soundBeer.volume = 0.1;
+let soundDonut = new Audio("./sound/mmm-donuts.mp3");
+soundDonut.volume = 0.1;
 
-  let soundHam = new Audio("./sound/mmm-ham.mp3");
-  soundHam.volume = 0.1;
+let soundBeer = new Audio("./sound/mmm-beer.mp3");
+soundBeer.volume = 0.1;
 
-  let soundDeath = new Audio("./sound/argh-death.mp3");
-  soundDeath.volume = 0.1;
+let soundHam = new Audio("./sound/mmm-ham.mp3");
+soundHam.volume = 0.1;
 
-  let soundPomatoJuice = new Audio("./sound/doh.mp3");
-  soundPomatoJuice.volume = 0.1;
+let soundDeath = new Audio("./sound/argh-death.mp3");
+soundDeath.volume = 0.1;
+
+let soundPomatoJuice = new Audio("./sound/doh.mp3");
+soundPomatoJuice.volume = 0.1;
